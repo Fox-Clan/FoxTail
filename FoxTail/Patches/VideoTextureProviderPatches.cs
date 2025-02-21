b23de09b6ba1b2b91085c11e28e42d8eb8ce35d8
jvyden <jvyden@jvyden.xyz>
2025-02-20 19:28:43 -0500

    Always use system lookup for yt-dlp

---
+++ FoxTail/Patches/VideoTextureProviderPatches.cs
@@ -0,0 +1,32 @@
+﻿using System.Reflection;
+using FrooxEngine;
+using HarmonyLib;
+using NYoutubeDL;
+using NYoutubeDL.Helpers;
+
+namespace FoxTail.Patches;
+
+[HarmonyPatch]
+public static class VideoTextureProviderPatches
+{
+    private static readonly MethodInfo GetFullPathInfo = AccessTools.Method(typeof(Extensions), "GetFullPath");
+    
+    [HarmonyPatch(typeof(VideoTextureProvider), "LoadFromVideoServiceIntern"), HarmonyPrefix]
+    public static bool LoadFromVideoServiceInternPrefix(ref YoutubeDL ___youtubeDL)
+    {
+        if (___youtubeDL == null)
+        {
+            string? fullPath = (string?)GetFullPathInfo.Invoke(null, [new FileInfo("yt-dlp")]);
+            if (fullPath == null)
+            {
+                Program.Context.Logger.LogWarning(ResoCategory.Runner, "yt-dlp not found!");
+            }
+            else
+            {
+                Program.Context.Logger.LogDebug(ResoCategory.Runner, "yt-dlp found at " + fullPath);
+            }
+            ___youtubeDL = new YoutubeDL(fullPath);
+        }
+        return true;
+    }
+}
