d5d5576fdd6232f04e719d7835a75f620f23a157
jvyden <jvyden@jvyden.xyz>
2025-02-22 18:43:35 -0500

    Fix incorrect defaults for Websocket/Bunkum enabled

--- StargateNetwork/StargateConfiguration.cs
+++ StargateNetwork/StargateConfiguration.cs
@@ -3,6 +3,6 @@
 public class StargateConfiguration
 {
     public string WebsocketHostUrl { get; set; } = "ws://127.0.0.1:10060";
-    public bool WebsocketEnabled { get; set; } = false;
-    public bool BunkumEnabled { get; set; } = false;
+    public bool WebsocketEnabled { get; set; } = true;
+    public bool BunkumEnabled { get; set; } = true;
 }
