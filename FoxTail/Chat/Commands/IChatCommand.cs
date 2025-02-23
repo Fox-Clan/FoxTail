7e07f109c7d885a0668373b0d6064bae2672c329
jvyden <jvyden@jvyden.xyz>
2025-02-22 22:15:43 -0500

    Command interface

---
+++ FoxTail/Chat/Commands/IChatCommand.cs
@@ -0,0 +1,11 @@
+﻿using JetBrains.Annotations;
+
+namespace FoxTail.Chat.Commands;
+
+[UsedImplicitly(ImplicitUseTargetFlags.WithInheritors)]
+public interface IChatCommand
+{
+    public string Name { get; }
+    public string HelpText { get; }
+    public Task InvokeAsync(HeadlessContext context, IChatChannel channel, IChatUser user, string[] args);
+}
